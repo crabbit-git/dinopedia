f547e97c8ec726f888f5b3cba4272712aca526c8
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-24 19:47:21 +0100

    Indentation fix

--- client/src/components/DinoTimeline.js
+++ client/src/components/DinoTimeline.js
@@ -2,21 +2,21 @@ import DinoCard from "./DinoCard";
 
 const DinoTimeline = ({allDinosaurs}) => {
 
-const renderDinos = allDinosaurs.map(dino => {
-  let periodClass = dino.period;
-  let cardDirection = "right"
-  if (allDinosaurs.indexOf(dino) %2 === 0) {
-    cardDirection = "left";
-  }
-    return(
-      <DinoCard
-      dino={dino}
-      key={dino._id}
-      cardDirection={cardDirection}
-      periodClass={periodClass}
-      />
-    );
-});
+  const renderDinos = allDinosaurs.map(dino => {
+    let periodClass = dino.period;
+    let cardDirection = "right"
+    if (allDinosaurs.indexOf(dino) %2 === 0) {
+      cardDirection = "left";
+    }
+      return(
+        <DinoCard
+        dino={dino}
+        key={dino._id}
+        cardDirection={cardDirection}
+        periodClass={periodClass}
+        />
+      );
+  });
 
   return(
     <div className="jurassic-div">
@@ -25,4 +25,4 @@ const renderDinos = allDinosaurs.map(dino => {
   );
 }
 
-export default DinoTimeline
+export default DinoTimeline;
