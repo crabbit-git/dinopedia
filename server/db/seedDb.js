0e98171e4f50523a5d30b5322498dba228197534
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-24 14:34:26 +0100

    Inconsistencies in 'eats' and 'period' data corrected

--- server/db/seedDb.js
+++ server/db/seedDb.js
@@ -38,15 +38,25 @@ const promisedDinos = dinoNames.map(dinoName => {
 
 const stripDinos = dinosFromApi => {
   return dinosFromApi.map(dino => {
-      delete dino.shouldShowMap;
-      delete dino.refs;
-      dino.pics.forEach(pic => {
-        delete pic.votingUrl;
-        delete pic.clickthrough_url;
-        delete pic.thumbnail;
-      });
-      return dino;
+    if (dino.eats === '') {
+      dino.eats = '(unknown)'
+    }
+    if (dino.period.includes('/')) {
+      const splitPeriod = dino.period.split('/');
+      dino.period = splitPeriod[0];
+    } else if (dino.period.includes('Early') || dino.period.includes('Late')) {
+      const splitPeriod = dino.period.split(' ');
+      dino.period = splitPeriod[1];
+    }
+    delete dino.shouldShowMap;
+    delete dino.refs;
+    dino.pics.forEach(pic => {
+      delete pic.votingUrl;
+      delete pic.clickthrough_url;
+      delete pic.thumbnail;
     });
+    return dino;
+  });
 }
 
 Promise.all(promisedDinos)
