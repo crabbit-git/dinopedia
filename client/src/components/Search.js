7aa51e5e3b87f88734a2022cf75a2b6d826f0936
sjohns2020 <98856218+sjohns2020@users.noreply.github.com>
2022-06-25 19:21:09 +0100

    fucking Jquery

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,17 +1,32 @@
 import './Search.css'
-import { useState } from 'react';
-import Autocomplete from "./Autocomplete";
+import $ from 'jquery'
+// import Autocomplete from "./Autocomplete";
 
 const Search = ({allDinosaurs}) => {
     
-   
-    const DinosaurNames = allDinosaurs.map( (dino) => dino.name )
 
+    $("#search").keyup(function(){
+      $("#background-input").val('');
+      const dinosaurNames = allDinosaurs.map( (dino) => dino.name )
+      const searchText = $("#search").val();
+      console.log(searchText.toLowerCase());
+      if(searchText === "")
+        $("#background-input").val('');
+      else if(dinosaurNames.indexOf(searchText.toLowerCase())===0)
+        {
+          const currentLength = searchText.length;
+          $("#background-input").val(searchText+""+dinosaurNames.forEach((dino)=> dino.substring(currentLength)));     
+        }
+    });
 return ( 
-  <div>
-    <h2>Search</h2>
-    <Autocomplete suggestions={DinosaurNames} />
-  </div>
+  // <div>
+  //   <h2>Search</h2>
+  //   <Autocomplete suggestions={DinosaurNames} />
+  // </div>
+  <div id="search-div">
+  <input type="text" id="background-input" />
+  <input type="text" id="search" placeholder="Type name of dino.." />
+</div>
 );
 }
  
