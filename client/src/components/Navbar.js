f164d0f88a5ed2c3359edaec90be4e1569d49331
sjohns2020 <98856218+sjohns2020@users.noreply.github.com>
2022-06-25 11:51:13 +0100

    Navbar container created

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,72 +1,10 @@
 
-import styles from './Navbar.css'
-import { useState, useRef, useEffect } from 'react';
+import './Navbar.css'
+import Search from './Search';
 
 const Navbar = ({allDinosaurs}) => {
 
-    const [query, setQuery] = useState();
 
-    const results = allDinosaurs.filter(({ name }) => query && name.toLowerCase().includes(query.toLowerCase()) );
-    const hasResults = results && results.length > 0;
-  
-    const inputRef = useRef();
-    const resultsRef = useRef();
-  
-    useEffect(() => {
-      inputRef.current.focus();
-    }, []);
-  
-    useEffect(() => {
-      if ( hasResults ) {
-        document.body.addEventListener('keydown', onKeyDown);
-      } else {
-        document.body.removeEventListener('keydown', onKeyDown);
-      }
-      return () => {
-        document.body.removeEventListener('keydown', onKeyDown);
-      }
-    }, [hasResults]);
-  
-    function onKeyDown(event) {
-      const isUp = event.key === 'ArrowUp';
-      const isDown = event.key === 'ArrowDown';
-      const inputIsFocused = document.activeElement === inputRef.current;
-  
-      const resultsItems = Array.from(resultsRef.current.children)
-  
-      const activeResultIndex = resultsItems.findIndex(child => {
-        return child.querySelector('a') === document.activeElement;
-      });
-  
-      if ( isUp ) {
-        console.log('Going up!');
-        if ( inputIsFocused ) {
-          resultsItems[resultsItems.length - 1].querySelector('a').focus();
-        } else if ( resultsItems[activeResultIndex - 1] ) {
-          resultsItems[activeResultIndex - 1].querySelector('a').focus();
-        } else {
-          inputRef.current.focus();
-        }
-      }
-  
-      if ( isDown ) {
-        console.log('Going down!')
-        if ( inputIsFocused ) {
-          resultsItems[0].querySelector('a').focus();
-        } else if ( resultsItems[activeResultIndex + 1] ) {
-          resultsItems[activeResultIndex + 1].querySelector('a').focus();
-        } else {
-          inputRef.current.focus();
-        }
-      }
-    }
-  /**
-   * handleOnChange
-   */
-
-   function handleOnChange(event) {
-    setQuery(event.currentTarget.value);
-  }
 
    
     
@@ -80,24 +18,7 @@ const Navbar = ({allDinosaurs}) => {
             <form >
                 <input type="text" />
             </form>
-            <form className={styles.form} method="post" autoComplete="off">
-          <input ref={inputRef} type="search" name="query" onChange={handleOnChange} />
-          {hasResults && (
-            <div className={styles.autocomplete}>
-              <ul ref={resultsRef} className={styles.people}>
-                {results.map(result => {
-                  return (
-                    <li key={result.url}>
-                      <a href={result.url}>
-                        { result.name }
-                      </a>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-          )}
-        </form>
+            <Search allDinosaurs={allDinosaurs}  />
             </div>
     );
 }
