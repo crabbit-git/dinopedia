c093e72aae91c562d0a12c8284fbf589c1f649b5
HeatherLamont <heatherlamontmusic@hotmail.co.uk>
2022-06-25 15:07:34 +0100

    created dino pop-up modal

--- client/src/components/DinoCard.js
+++ client/src/components/DinoCard.js
@@ -1,27 +1,71 @@
+import React, { useState } from "react";
+import { Styles } from "react-modal";
+import styles from "./DinoModal.css";
+
+
+import Modal from "react-modal";
+Modal.setAppElement("#root");
+
 const DinoCard = ({dino, cardDirection, periodClass}) => {
   const containerClass = `container ${cardDirection}`;
   const timelineClass = `timeline ${periodClass}`
+  const mapClass = `https://dinosaurpictures.org/${dino.mapUrl}`
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleModal() {
+    setIsOpen(!isOpen);
+  }
+
+
+
+
+
   return (
-    <div id={dino.name} className={timelineClass}>
-      <div className={containerClass}>
+    <div  id={dino.name} className={timelineClass}>
+      <div onClick={toggleModal} className={containerClass}>
         <div className="date">{dino.period}</div>
         <i className="icon fa fa-home"/>
         <div className="content">
-          <img
+          <img 
               className="dino-image"
               src={dino.pics[0].url}
               alt={dino.name}
               width="300"
           />
           <h2>{dino.name}</h2>
-          <p>
+          
+        </div>
+      </div>
+      <Modal isOpen={isOpen}
+        onRequestClose={toggleModal}
+        contentLabel="modal"
+        className="dino-modal-content"
+        // overlayClassName="dino-modal-overlay"
+        >
+          <img 
+              className="dino-image"
+              src={dino.pics[0].url}
+              alt={dino.name}
+              width="300"
+              />
+          <button onClick={toggleModal} type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon-remove"></span> X 
+        </button>
+        
+              <h2>{dino.name}</h2>
+        <p>
             <b>Diet:</b> {dino.eats}
             <br/>
             <b>Found in:</b> {dino.regions.join(", ")}
+            <br/>
+            <img src={mapClass} alt="" />
+            
           </p>
-        </div>
-      </div>
+      </Modal>
     </div>
+
+
   );
 }
 
