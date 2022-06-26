6fceff4fe3ba63fc31fd1cba05d3f1f447ff2a5b
HeatherLamont <heatherlamontmusic@hotmail.co.uk>
2022-06-26 14:35:38 +0100

    Add routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import {useEffect, useState} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import DinoTimeline from './components/DinoTimeline';
 import Navbar from './components/Navbar';
 
+
 function App() {
 
   const [allDinosaurs, setAllDinosaurs] = useState([]);
+  // const [favDinosaurs, setFavDinosaurs] = useState([]);
+
+
+  // [...favDinosaurs]
 
   useEffect(() => {
     fetch('http://localhost:5000/api/dinosaurs/')
@@ -14,11 +20,16 @@ function App() {
   }, [])
 
   return (
-    <div>
+    <BrowserRouter>
       <Navbar allDinosaurs={allDinosaurs}/>
       <h1>Butts</h1>
-      <DinoTimeline allDinosaurs={allDinosaurs}/>
-    </div>
+      <Routes>
+        <Route path="/" element={<DinoTimeline allDinosaurs={allDinosaurs}/>}/>
+        <Route path="/favourites" element={<DinoTimeline/>}/>
+      </Routes>
+      {/* <DinoTimeline allDinosaurs={allDinosaurs}/> */}
+    </BrowserRouter>
+   
   );
 }
 
