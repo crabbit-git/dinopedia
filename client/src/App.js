94e5c5f93de0c0c0d0392fea256d13caa28befbf
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-27 10:27:25 +0100

    WIP localStorage persistence of favs

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,10 @@ import './App.css';
 import DinoTimeline from './components/DinoTimeline';
 import Navbar from './components/Navbar';
 
-
 function App() {
 
   const [allDinosaurs, setAllDinosaurs] = useState([]);
-  // const [favDinosaurs, setFavDinosaurs] = useState([]);
-
+  const [favDinosaurs, setFavDinosaurs] = useState([]);
 
   // [...favDinosaurs]
 
@@ -17,19 +15,38 @@ function App() {
     fetch('http://localhost:5000/api/dinosaurs/')
     .then(res => res.json())
     .then(data => setAllDinosaurs(data))
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    // let tempFavs = [];
+    const savedFavs = window.localStorage.getItem('favDinoNames');
+    if (savedFavs) {
+      // tempFavs = savedFavs
+      setFavDinosaurs(
+        allDinosaurs.filter(dino => savedFavs.includes(dino.name.toLowerCase()))
+      );
+    }
+  }, [allDinosaurs]);
+
+  // useEffect(() => {
+    
+  // }, [favDinosaurs]);
 
   return (
     <BrowserRouter>
       <Navbar allDinosaurs={allDinosaurs}/>
       <h1>Butts</h1>
       <Routes>
-        <Route path="/" element={<DinoTimeline allDinosaurs={allDinosaurs}/>}/>
-        <Route path="/favourites" element={<DinoTimeline/>}/>
+        <Route
+          path="/"
+          element={<DinoTimeline allDinosaurs={allDinosaurs}/>}
+        />
+        <Route
+          path="/favourites"
+          element={<DinoTimeline allDinosaurs={favDinosaurs}/>}
+        />
       </Routes>
-      {/* <DinoTimeline allDinosaurs={allDinosaurs}/> */}
     </BrowserRouter>
-   
   );
 }
 
