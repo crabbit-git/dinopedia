5b3e922dc9e3808778029319168caa1c81f1fba0
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-28 01:47:46 +0100

    Indentation etc.

--- client/src/components/DinoTimeline.js
+++ client/src/components/DinoTimeline.js
@@ -1,36 +1,32 @@
 import DinoCard from "./DinoCard";
 
 const DinoTimeline = (
-  {allDinosaurs, handleAddFavDino, handleRemoveFavDino, favDinosaurs}
+  {allDinosaurs, favDinosaurs, handleAddFavDino, handleRemoveFavDino}
 ) => {
   const renderDinos = () =>
     [...allDinosaurs]
     .sort((a,b) => a.name.localeCompare(b.name))
     .sort((a,b) => b.era.localeCompare(a.era))
     .map((dino, index) => {
-
-    const periodClass = dino.period;
-    let cardDirection = "right"
-    let isFavourite = favDinosaurs.some(favDino => {
-        return favDino._id === dino._id
+      let cardDirection = "right"
+      if (index %2 === 0) {
+        cardDirection = "left";
+      }
+      let isFavourite = favDinosaurs.some(favDino => {
+          return favDino._id === dino._id
+      });
+      return (
+        <DinoCard
+          dino={dino}
+          key={dino._id}
+          cardDirection={cardDirection}
+          periodClass={dino.period}
+          handleAddFavDino={handleAddFavDino}
+          handleRemoveFavDino={handleRemoveFavDino}
+          isFavourite={isFavourite}
+        />
+      );
     });
-
-    if (index %2 === 0) {
-      cardDirection = "left";
-    }
-
-    return (
-      <DinoCard
-        dino={dino}
-        key={dino._id}
-        cardDirection={cardDirection}
-        periodClass={periodClass}
-        handleAddFavDino={handleAddFavDino}
-        handleRemoveFavDino={handleRemoveFavDino}
-        isFavourite={isFavourite}
-      />
-    );
-  });
 
   return (
     <div className="timeline-container">
