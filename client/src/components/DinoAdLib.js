bdc2e287c372bfdbd3770d3d421f69b6be5d0575
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-30 00:22:17 +0100

    DinoAdLib now uses verb; tidied up redundant crap

--- client/src/components/DinoAdLib.js
+++ client/src/components/DinoAdLib.js
@@ -1,57 +1,55 @@
 import React, { useState } from "react";
-import { Styles } from "react-modal";
-import styles from "./LandingModal.css";
-
 import Modal from "react-modal";
+
 Modal.setAppElement("#root");
 
 const DinoAdLibs = () => {
 
   const [isOpen, setIsOpen] = useState(true);
   const [isOpen2, setIsOpen2] = useState(true);
-  const [dinoName, setDinoName] = useState('') 
+  const [noun0, setNoun0] = useState('') 
   const [verb, setVerb] = useState('')
-  const [noun, setNoun] = useState('')
+  const [noun1, setNoun1] = useState('')
   const [noun2, setNoun2] = useState('')
-  const [Adjective, setAdjective] = useState('')
+  const [adjective, setAdjective] = useState('')
 
-  function toggleModal1() {
+  const toggleModal1 = () => {
     setIsOpen(!isOpen);
   }
-  function toggleModal2() {
+  const toggleModal2 = () => {
     setIsOpen2(!isOpen2);
   }
-  function handleSubmit() {
+  const handleSubmit = () => {
     setIsOpen2(!isOpen2);
   }
-  function handleCloseAll() {
+  const handleCloseAll = () => {
     setIsOpen2(!isOpen2);
     setIsOpen(!isOpen); 
   }
 
-  const handleDinoNameChange = (event) => {
-      setDinoName(event.target.value)
+  const handleNoun0Change = event => {
+      setNoun0(event.target.value)
   }
-  const handleVerbChange = (event) => {
+  const handleVerbChange = event => {
       setVerb(event.target.value)
   }
-  const handleNounChange = (event) => {
-      setNoun(event.target.value)
+  const handleNoun1Change = event => {
+      setNoun1(event.target.value)
   }
-  const handleNoun2Change = (event) => {
+  const handleNoun2Change = event => {
       setNoun2(event.target.value)
   }
-  const handleAdjectiveChange = (event) => {
+  const handleAdjectiveChange = event => {
       setAdjective(event.target.value)
   }
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = event => {
     event.preventDefault();
     if (
-      dinoName === "" ||
-      noun === "" ||
-      verb  === "" ||
+      noun0 === "" ||
+      noun1 === "" ||
       noun2 === "" ||
-      Adjective === ""
+      verb  === "" ||
+      adjective === ""
     ) {
       alert("Please complete all fields");
       toggleModal2();
@@ -60,7 +58,7 @@ const DinoAdLibs = () => {
     return true;
   }
 
-  const here = <a
+  const natGeoLink = <a
       className="adlib-atag"
       href="https://www.nationalgeographic.com/science/article/dinosaur-killing-asteroid-most-likely-struck-in-spring#:~:text=The%20impact%20that%20ended%20the,75%20percent%20of%20Earth's%20species."
     >here</a>
@@ -82,31 +80,18 @@ const DinoAdLibs = () => {
         >
         &times;
         </button>
-        <h3 className="adlib-form-title">Discover why dinosaurs are extinct and practice your grammar</h3>
+        <h3 className="adlib-form-title">
+          Learn about why dinosaurs are extinct...<br/>
+          and practice your grammar!
+        </h3>
         <form onSubmit={handleFormSubmit} className="adlib-form">
-          <label className="adlib-input" htmlFor="dinoName">
-            Enter a noun (a "naming" word):
+        <label className="adlib-input" htmlFor="noun1">
+            Enter a noun ("naming" word):
           </label>
           <input
-            type="text" id="dinoName" name="dinoName"
+            type="text" id="noun1" name="noun1"
             className="adlib-input-box"
-            value={dinoName} onChange={handleDinoNameChange}
-          />
-          <label className="adlib-input" htmlFor="verb">
-            Enter a verb (a "doing" word):
-          </label>
-          <input
-            type="text" id="verb" name="verb"
-            className="adlib-input-box"
-            value={verb} onChange={handleVerbChange}
-          />
-          <label className="adlib-input" htmlFor="noun">
-            Enter another noun:
-          </label>
-          <input
-            type="text" id="noun" name="noun"
-            className="adlib-input-box"
-            value={noun} onChange={handleNounChange}
+            value={noun1} onChange={handleNoun1Change}
           />
           <label className="adlib-input" htmlFor="noun2">
             Enter another noun:
@@ -115,13 +100,29 @@ const DinoAdLibs = () => {
           type="text" id="noun2" name="noun2" className="adlib-input-box"
           value={noun2} onChange={handleNoun2Change}
           />
-          <label className="adlib-input" htmlFor="Adjective">
+          <label className="adlib-input" htmlFor="noun0">
+            Enter a proper noun ("naming" word starting with a capital letter):
+          </label>
+          <input
+            type="text" id="noun0" name="noun0"
+            className="adlib-input-box"
+            value={noun0} onChange={handleNoun0Change}
+          />
+          <label className="adlib-input" htmlFor="verb">
+            Enter a verb ("doing" word):
+          </label>
+          <input
+            type="text" id="verb" name="verb"
+            className="adlib-input-box"
+            value={verb} onChange={handleVerbChange}
+          />
+          <label className="adlib-input" htmlFor="adjective">
             Enter an adjective ("describing" word):
           </label>
           <input
-            type="text" id="Adjective" name="Adjective"
+            type="text" id="adjective" name="adjective"
             className="adlib-input-box1"
-            value={Adjective} onChange={handleAdjectiveChange}
+            value={adjective} onChange={handleAdjectiveChange}
           />
           <button
             type="submit" onClick={handleSubmit}
@@ -152,17 +153,18 @@ const DinoAdLibs = () => {
             <h3 className="adlib-return-title">A long time ago...</h3>
            <p className="adlib-return">
             There once was a dinosaur called
-            <span className="adlib-css"> {dinoName} </span>
-            who lived in the cretaceous era and loved to eat
-            <span className="adlib-css"> {noun}</span>.
+            <span className="adlib-css"> {noun0} </span>
+            who lived in the cretaceous era and loved to
+            <span className="adlib-css"> {verb}</span>
+            <span className="adlib-css"> {noun1}</span>.
             <br /><br />
             Unfortunately, about 66 million years ago,
-            <span className="adlib-css"> {dinoName} </span>
+            <span className="adlib-css"> {noun0} </span>
             was wiped out with the rest of the dinosaurs by a
-            <span className="adlib-css"> {Adjective}</span>
+            <span className="adlib-css"> {adjective}</span>
             <span className="adlib-css"> {noun2} </span>
             which struck Earth, causing a
-            <span className="adlib-css"> {Adjective} </span>
+            <span className="adlib-css"> {adjective} </span>
             extinction event which ended the
             cretaceous era and the mesozoic era as a whole (the mesozoic era was
             made up of the triassic, jurassic, and cretaceous eras).
@@ -173,14 +175,14 @@ const DinoAdLibs = () => {
             <span className="adlib-css"> {noun2} </span>
             called Chicxulub slammed into the waters off an area we now know as
             Mexico, triggering a
-            <span className="adlib-css"> {Adjective} </span>
+            <span className="adlib-css"> {adjective} </span>
             extinction that killed off more than 75 percent of Earth's species.
             <br /><br />
             Wait a minute... that's not quite right, is it?
             <br /><br />
             If you want to know what really happened to
-            <span className="adlib-css"> {dinoName}</span>, click
-            <span className="adlib-css"> {here}</span>!
+            <span className="adlib-css"> {noun0}</span>, click
+            <span className="adlib-css"> {natGeoLink}</span>!
            </p>
             <br/>
             <button
