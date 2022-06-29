76d599442d256e7dafdbdb01a5941c173c6fa58b
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-29 14:52:18 +0100

    Audio.js tidied up

--- client/src/components/Audio.js
+++ client/src/components/Audio.js
@@ -1,17 +1,8 @@
-
-//in dino timeline   
-// const url = 'https://www.nhm.ac.uk/content/dam/nhmwww/take-part/dippy-on-tour/jurassic-soundscape.mp3'
-// <Player url={url}/>
-
-//In seperate component
 import React, { useState, useEffect } from "react";
-
 
 const useAudio = url => {
   const [audio] = useState(new Audio(url));
   const [playing, setPlaying] = useState(false);
-
-
 
   const toggle = () => setPlaying(!playing);
 
@@ -31,19 +22,21 @@ const useAudio = url => {
   return [playing, toggle];
 };
 
-const Player = ({ url }) => {
-  const [playing, toggle] = useAudio('https://www.nhm.ac.uk/content/dam/nhmwww/take-part/dippy-on-tour/jurassic-soundscape.mp3');
-
+const Player = ({url}) => {
+  const [playing, toggle] = useAudio(
+    "https://www.nhm.ac.uk/content/dam/nhmwww/take-part/dippy-on-tour/jurassic-soundscape.mp3"
+  );
 
   return (
-
       <div>
-        <img onClick={toggle} id="logo-pic" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/322/sauropod_1f995.png" alt="dino logo"/> 
+        <img
+          onClick={toggle} id="logo-pic"
+          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/322/sauropod_1f995.png"
+          alt="dino logo"
+        /> 
         {playing}
       </div>
-  
       );
     };
-
 
 export default Player;
