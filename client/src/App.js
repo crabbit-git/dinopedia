c76cf52bd64d82ce64373f6ec886d59514c31acd
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-29 16:56:10 +0100

    App.js tidied a bit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import {useEffect, useState} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import DinoTimeline from './components/DinoTimeline';
-import Navbar from './components/Navbar';
 import LandingModal from './components/LandingModal';
+import Navbar from './components/Navbar';
+import DinoTimeline from './components/DinoTimeline';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
@@ -14,8 +14,7 @@ function App() {
   const [creators, setCreators] = useState([])
   const savedFavs = window.localStorage.getItem('favDinoNames');
 
-
-  // Load in dinosaurs from database at startup and render page
+  // Load in dinosaurs from database at startup
   useEffect(() => {
     fetch('http://localhost:5000/api/dinosaurs/')
     .then(res => res.json())
@@ -28,24 +27,24 @@ function App() {
     );
   }, []);
 
-  // fetch all random facts from Db
+  // Load in random facts from database at startup
   useEffect(() => {
     fetch('http://localhost:5000/api/randomFacts/')
     .then(res => res.json())
     .then(data => {
       setRandomFacts(data);
-    })}, [])
-    
-    useEffect(() => {
+    });
+  }, []);
+
+  // Load in Team Dinomite data from database at startup
+  useEffect(() => {
     fetch('http://localhost:5000/api/creators/')
     .then(res => res.json())
     .then(data => {
       setCreators(data);
       console.log("facts data", data);
-    })}, [])
-
-  // console.log(creators);
-  // console.log(randomFacts);
+    });
+  }, []);
 
   // When dinosaur list is populated, check for saved favourites in localStorage
   // and if they exist, pull them into the application's favourites list:
@@ -68,27 +67,28 @@ function App() {
     }
   }, [favDinosaurs]);
 
-
   const handleAddFavDino = dinoId => {
     const foundFavDino = allDinosaurs.find(dinosaur => {
       return dinosaur._id === dinoId // comparing the ID object to the ID string
     });
-  setFavDinosaurs([...favDinosaurs, foundFavDino]);
+    setFavDinosaurs([...favDinosaurs, foundFavDino]);
   }
 
   const handleRemoveFavDino = dinoId => {
     const filteredFavDinos = favDinosaurs.filter(favDinosaur => {
       return favDinosaur._id !== dinoId
     });
-  setFavDinosaurs(filteredFavDinos);
+    setFavDinosaurs(filteredFavDinos);
   }
-
-
 
   return (
     <BrowserRouter>
       <LandingModal />
-      <Navbar allDinosaurs={allDinosaurs} creators={creators}  randomFacts={randomFacts} />
+      <Navbar
+        allDinosaurs={allDinosaurs}
+        creators={creators}
+        randomFacts={randomFacts}
+      />
       <ScrollToTop/>
       <Routes>
         <Route
@@ -119,5 +119,3 @@ function App() {
 }
 
 export default App;
-
-
