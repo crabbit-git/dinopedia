00c5ecafab4bdc324645de596cc0ff1ed8ff460a
sjohns2020 <98856218+sjohns2020@users.noreply.github.com>
2022-06-29 01:27:25 +0100

    sean moving to xanders branch

--- client/src/components/DinoAdLib.js
+++ client/src/components/DinoAdLib.js
@@ -10,7 +10,7 @@ const DinoAdLibs = () => {
   
     const [isOpen, setIsOpen] = useState(true);
     const [isOpen2, setIsOpen2] = useState(true);
-    const [dinoName, setDinoName] = useState('')
+    const [dinoName, setDinoName] = useState('') 
     const [verb, setVerb] = useState('')
     const [noun, setNoun] = useState('')
     const [noun2, setNoun2] = useState('')
@@ -37,6 +37,7 @@ const DinoAdLibs = () => {
 
     const handleDinoNameChange = (event) => {
         setDinoName(event.target.value)
+        
     }
 
     const handleVerbChange = (event) => {
@@ -61,7 +62,7 @@ const DinoAdLibs = () => {
         return false;
     }}
 
-    const here =  <a href="https://www.nationalgeographic.com/science/article/dinosaur-killing-asteroid-most-likely-struck-in-spring#:~:text=The%20impact%20that%20ended%20the,75%20percent%20of%20Earth's%20species.">here.</a>
+    const here =  <a className="adlib-atag" href="https://www.nationalgeographic.com/science/article/dinosaur-killing-asteroid-most-likely-struck-in-spring#:~:text=The%20impact%20that%20ended%20the,75%20percent%20of%20Earth's%20species.">here.</a>
 
   
   return (
@@ -75,23 +76,24 @@ const DinoAdLibs = () => {
         contentLabel="modal"
         className="landing-modal-content"
       >
-        <h3>Dino AdLibs From</h3>
+        <h3>Dinopedia Ad-libs</h3>
+        <h3>Discover why dinosaurs are extinct and practice your grammar</h3>
         <form onSubmit={handleFormSubmit} className="adlib-form">
-    
-      <label htmlFor="dinoName">dinoName:</label>
-      <input type="text" id="dinoName" name="dinoName" value={dinoName} onChange={handleDinoNameChange} />
 
-      <label htmlFor="verb">A Verb or doing word:</label>
-      <input type="text" id="verb" name="verb" value={verb} onChange={handleVerbChange} />
+      <label  className="adlib-input" htmlFor="dinoName">A noun or naming word:</label>
+      <input type="text" id="dinoName" name="dinoName" className="adlib-input-box" value={dinoName} onChange={handleDinoNameChange} />
 
-      <label htmlFor="noun">A noun or naming word:</label>
-      <input type="text" id="noun" name="noun" value={noun} onChange={handleNounChange} />
+      <label className="adlib-input" htmlFor="verb">A Verb or doing word:</label>
+      <input type="text" id="verb" name="verb" className="adlib-input-box" value={verb} onChange={handleVerbChange} />
 
-      <label htmlFor="noun2">Another noun or naming word:</label>
-      <input type="text" id="noun2" name="noun2" value={noun2} onChange={handleNoun2Change} />
+      <label className="adlib-input" htmlFor="noun">A noun or naming word:</label>
+      <input type="text" id="noun" name="noun"className="adlib-input-box" value={noun} onChange={handleNounChange} />
 
-      <label htmlFor="Adjective">An Adjective or describing word:</label>
-      <input type="text" id="Adjective" name="Adjective" value={Adjective} onChange={handleAdjectiveChange} />
+      <label className="adlib-input" htmlFor="noun2">Another noun or naming word:</label>
+      <input type="text" id="noun2" name="noun2" className="adlib-input-box"value={noun2} onChange={handleNoun2Change} />
+
+      <label className="adlib-input" htmlFor="Adjective">An Adjective or describing word:</label>
+      <input type="text" id="Adjective" name="Adjective" className="adlib-input-box1"value={Adjective} onChange={handleAdjectiveChange} />
 
         <button type="submit" onClick={handleSubmit} className="landing-modal-button" >
         Submit Answers
@@ -108,13 +110,15 @@ const DinoAdLibs = () => {
         className="landing-modal-content"
       >
         <h3>Dino AdLibs Final</h3>
-        <p>There once was a dinosaur called {dinoName} who lived in the triassic period and mainly ate
-        {noun}.  {dinoName} was wiped out with the rest of the dinosaurs by a 
-        {Adjective} {noun2} which struck Earth. The impact that ended the age of dinosaurs some 66 million 
-        years ago was the worst single day that life on Earth has ever endured. A six-mile-wide 
-        {noun2} called Chicxulub slammed into the waters off what is now Mexico, triggering a 
-        {Adjective} extinction that killed off more than 75 percent of Earth's species. No wait that is
-         not right??? If you really want ot know what happened to {dinoName} click {here} </p>
+        <p className="adlib-return" >There once was a dinosaur called <span > {dinoName} </span> who lived in the triassic period and mainly ate
+        <span className="adlib-css"> {noun} </span> .  
+        <br /> <br /><span className="adlib-css"> {dinoName}  </span> was wiped out with the rest of the dinosaurs by a 
+        <span className="adlib-css"> {Adjective} </span> <span className="adlib-css"> {noun2}  </span> which struck Earth. <br /> <br /> The impact that ended the age of dinosaurs some 66 million 
+        years ago was the worst single day that life on Earth has ever endured. <br /> <br /> A six-mile-wide 
+        <span className="adlib-css"> {noun2}  </span> called Chicxulub slammed into the waters off what is now Mexico, triggering a 
+        <span className="adlib-css"> {Adjective}  </span> extinction that killed off more than 75 percent of Earth's species. <br />  br
+        No wait that is
+         not right??? If you really want ot know what happened to <span className="adlib-css"> {dinoName} </span>  then just click <span className="adlib-css"> {here}  </span> </p>
      <button type="button"  onClick={handleCloseAll} className="landing-modal-close-button" >
       &times;
       </button>
